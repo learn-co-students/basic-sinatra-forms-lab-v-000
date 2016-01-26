452209260b371b4656fa52fea01c6ffd53cb0832
c1505 <coreym247@gmail.com>
2016-01-25 22:00:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
+require_relative 'config/environment'
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
+
+  post '/team' do 
+    
+    @team_name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @center = params["c"]
+    erb :team
+  end
 
 end
