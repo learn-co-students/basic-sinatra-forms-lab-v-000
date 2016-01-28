ef7a88e85bb2b38fd07c4eec115ae8289313bebb
febbraiod <donedesign1@gmail.com>
2016-01-28 18:20:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/team" do
+    erb :team
+  end
+
+  post '/team' do
+    erb :team
+  end
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+
 
 end
