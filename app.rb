e13a84032e019e291b2b59f97cb72524dea6e567
pajamaw <pjpeterww@gmail.com>
2016-02-08 13:19:24 -0500

    Working as it is supposed to. Couple issues with it. had to add require 'capybara/dsl' to allow tests to run. then had to change the tests to be correct with what the readme asked.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/team' do 
+    erb :team
+  end
 
+  post '/team' do 
+    erb :newteam
+  end
+
+  post '/newteam' do 
+    erb :newteam
+  end
 end
