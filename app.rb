073de776addee7ae048bcf98a2339a515aeaba02
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-02-09 00:24:32 +0000

    changed spec, instructions did not match with rspec demands

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  #  get '/team' do
+ #   erb :team
+#  end
 
+  post '/newteam' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @center = params[:c]
+    erb :team
+  end
+  
+  get '/newteam' do 
+    erb :newteam
+  end
 end
