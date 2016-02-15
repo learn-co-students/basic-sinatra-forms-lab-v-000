4c0558f18a863004db5226dfd9ca6c651da48453
tuckerbohman5 <tucker.bohman@gmail.com>
2016-02-15 15:47:19 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+    
+    
+  end
+
+  post '/newteam' do 
+    @params = params
+    erb :team
+  end
+
 
 end
