4e0e9ecab1e0ed9a84fa465515783609e3b485c0
Jacob <mailme711@gmail.com>
2016-02-22 10:50:07 -0500

    Revert "troubleshooting"
    This reverts commit 8dc23a4f00c61496572d3ba04321bfcec9746633.

--- app.rb
+++ app.rb
@@ -2,20 +2,5 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get "/team" do
-    erb :team
-  end
-
-  post "/team" do
-    erb :newteam
-      @name = params[:name]
-      @coach = params[:coach]
-      @pg = params[:pg]
-      @sg = params[:sg]
-      @sf = params[:sf]
-      @pf = params[:pf]
-      @c = params[:c]
-  end
-
 
 end
