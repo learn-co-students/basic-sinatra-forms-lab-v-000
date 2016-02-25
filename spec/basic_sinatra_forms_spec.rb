dcd609a729a43f6f0735afe4ec310a3fb45a1f13
zacscodingclub <zbaston@gmail.com>
2016-02-24 21:04:40 -0600

    Done.

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -67,10 +67,10 @@ describe App do
     it "displays the shooting gaurd's name in the browser" do
       visit '/newteam'
 
-      fill_in(:sg, :with => "Joe")
+      fill_in(:sf, :with => "Joe")
       click_button "submit"
 
-      expect(page).to have_text("Shooting Guard: Joe")
+      expect(page).to have_text("Small Forward: Joe")
     end
 
     it "displays the center's name in the browser" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV["SINATRA_ENV"] = "test"
 require_relative '../config/environment'
 require 'rack/test'
 require 'capybara/poltergeist'
+require 'capybara/rspec'
 
 Capybara.javascript_driver = :poltergeist
 
