1ffb309c329b24bc47b02af3d6878b7d562b1ace
ed-karabinus <ejkarabin@gmail.com>
2016-03-06 13:56:28 -0600

    Finished Basic Sinatra Forms Lab

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do 
+    erb :team
+  end
 
+  post '/team' do 
+    @name = params[:name]
+    @coach = params[:coach]
+    @sg = params[:sg]
+    @pg = params[:pg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :newteam
+  end
 
 end
