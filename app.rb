18d4f980552fc9dbdf058736678dd40e80339944
jakeekaj <jakemalto2014@gmail.com>
2016-03-17 09:45:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
-require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @c = params[:c]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    erb :team
+  end
 
 end
