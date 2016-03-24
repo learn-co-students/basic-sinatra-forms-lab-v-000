8d1575ddd9809d344003a6dbbccbec7c2215b532
agdavid <antonio.g.david@gmail.com>
2016-03-24 10:04:56 -0400

    code basic GET and POST routes with forms in Sinatra

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello, this is the index."
+  end
+
+  get '/newteam' do
+    erb :team
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    erb :newteam
+  end
+
 
 end
