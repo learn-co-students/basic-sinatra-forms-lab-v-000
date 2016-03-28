44a2667b9264c15750f7e9afaef3d68a4761fa91
innatewonderer <jzueva@gmail.com>
2016-03-27 20:25:06 -0400

    cleanup leftover >>> HEAD from a merge conflict and add `app.rb` to specify routes

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    @team_members = %w(name coach pg sg pf sf c)
+    erb :newteam
+  end
+
+  post '/team' do
+    original_string = params['string']
+    @reversed_string = original_string.reverse
+    erb :newteam
+  end
+
 
 end
