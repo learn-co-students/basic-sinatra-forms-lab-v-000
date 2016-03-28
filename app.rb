28d70784ec6b2986683d8e36e63d29d089b0c27e
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-03-28 19:32:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+  get '/team' do
+    @team = params
+    erb :team
+  end
+
+  post '/team' do
+    @team = params
+    raise params
+    @team
+    erb :newteam
+  end
+
+  get '/newteam' do
+    raise params
+    erb :newteam
+  end
 
 end
