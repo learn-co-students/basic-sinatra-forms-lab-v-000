d3eaa3ef0cfbb1c57fdd7b3944f03a1db3513918
smithWEBtek <brad@smithwebtek.com>
2016-03-30 20:22:03 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,25 +1,14 @@
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get '/team' do
-    erb :team 
+  get '/newteam' do 
+    erb :team
   end
 
   post '/team' do
-    erb :newteam
-  end
-
-  post '/newteam' do
-    puts params
-    @team = "#{params[":name"]}"
-    @coach = "#{params[":coach"]}"
-    @pg = "#{params[":pg"]}"
-    @sg = "#{params[":sg"]}"
-    @pf = "#{params[":pf"]}"
-    @sf = "#{params[":sf"]}"
-    @c = "#{params[":c"]}"
-
+    @form_hash = params
     erb :newteam
   end
 
