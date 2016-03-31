d6c7dc517ebe2aa42f3bc02ed7ca27828070d27a
smithWEBtek <brad@smithwebtek.com>
2016-03-30 20:03:22 -0400

    solution follows README instructions and works, but fails all rspec tests

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/team' do
+    erb :team 
+  end
+
+  post '/team' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    puts params
+    @team = "#{params[":name"]}"
+    @coach = "#{params[":coach"]}"
+    @pg = "#{params[":pg"]}"
+    @sg = "#{params[":sg"]}"
+    @pf = "#{params[":pf"]}"
+    @sf = "#{params[":sf"]}"
+    @c = "#{params[":c"]}"
+
+    erb :newteam
+  end
 
 end
