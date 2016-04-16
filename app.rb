d3afcaae9d74e54e502c9b7c7ac17e544325ad97
depippo <jdepippo@gmail.com>
2016-04-16 16:35:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/team' do
+    erb :team
+  end
+
+  post '/team' do
+    erb :newteam
+  end
+
+  get '/newteam' do
+    erb :team
+  end
 
 end
