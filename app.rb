1226b594af4ea7e1d9b21f55285e8f8fe2b877c0
hangloos <ericloos00@gmail.com>
2016-06-11 17:32:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
+  
+  post '/team' do
+    @name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :team
+  end
+
 
 end
