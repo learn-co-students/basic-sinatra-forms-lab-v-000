f4693e600dd559d498966415424cdb7466ff79ec
Hiral Desai <hiral.desai83@gmail.com>
2016-06-16 06:03:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name_display = params[:name]
+    @coach_display = params[:coach]
+    @pointguard_display = params[:pg]
+    @shootinguard_display = params[:sg]
+    @powerforward_display = params[:pf]
+    @smallforward_display = params[:sf]
+    @center_display = params[:c]
+    erb :team
+  end
 
 end
