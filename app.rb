0aad02d037dd33061c9e635986ad3acf1308eaaa
kaileeagray <kaileeagray@gmail.com>
2016-06-24 13:06:24 -0500

    fixed errors and removed string reverse

--- app.rb
+++ app.rb
@@ -3,14 +3,12 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-    @team_members = %w(name coach pg sg pf sf c)
     erb :newteam
   end
 
   post '/team' do
-    original_string = params['string']
-    @reversed_string = original_string.reverse
-    erb :newteam
+    @data = params
+    erb :team
   end
 
 
