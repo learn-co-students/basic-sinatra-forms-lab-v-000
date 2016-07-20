b1e14b9cc3079bb112a10bcccbaa8e5591aefb5a
Jason Casares <jasonc@csdrafting.com>
2016-07-20 15:33:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  get '/team' do
+    erb :team
+  end
+
+  post '/team' do
+    erb :team
+  end
 
 end
