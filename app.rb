5f439ed4ddc85b2df4f60a7cefcb74a18d91dd09
Rebecca Sherman <beckysherman000@gmail.com>
2016-07-21 17:48:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    erb :team
+  end
 end
