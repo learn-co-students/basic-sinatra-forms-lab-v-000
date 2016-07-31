f771c45168223136e4c87d216525ad0d47a317b6
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-31 04:05:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    puts params
+    erb :newteam
+  end
+
+  post '/team' do
+    @newroster = params["string"]
+    erb :team
+  end
 
 end
