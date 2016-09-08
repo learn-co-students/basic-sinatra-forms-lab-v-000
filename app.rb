bc358e8d95dbfb53049f1b9fad1d734d66b0d88e
SnowboardTechie <bryan@snowboardtechie.com>
2016-09-07 18:04:06 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @coach = params[:coach]
+    @team_name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @center = params[:c]
+    erb :team
+  end
 
 end
