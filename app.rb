a564138eb70cc3250462f4f55a52296e66215bd0
Scott Savage <snsavage@gmail.com>
2016-10-28 11:45:49 -0400

    Pass all lab specs

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @team = Struct.new(:name, :coach, :pg, :sg, :pf, :sf, :c).new
+    params.each {|k, v| @team.send("#{k}=",v)}
+    erb :team
+  end
 end
