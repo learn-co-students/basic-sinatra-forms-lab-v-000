b841668c7636de969de63dbac996f587105d1717
Howard DeVennish <howarddevennish@gmail.com>
2016-11-15 20:04:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "I just like to have a / route, can you blame me?"
+  end
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
+  #  binding.pry
+    erb :team
+  end
 
 end
