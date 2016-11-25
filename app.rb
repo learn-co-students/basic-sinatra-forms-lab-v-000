1cd9c66cec692b8b9293dac04eadd27bf33e99fa
Adam Friedl <ajfriedl@gmail.com>
2016-11-25 12:22:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    @team_members = %w(name coach pg sg sm pf c)
+    erb :newteam
+  end
+
+  post '/team' do
+    erb :team
+  end
+
 
 end
