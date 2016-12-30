3ef9d2fb5b4162670bafb7ad7a901b137d588efd
Adam Lucia <lucia.adam@gmail.com>
2016-12-30 06:17:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @p_guard = params[:pg]
+    @s_guard = params[:sg]
+    @s_forward = params[:sf]
+    @p_forward = params[:pf]
+    @center = params[:c]
+    
+    erb :team
+  end
 
 end
