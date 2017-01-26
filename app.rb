ac077910156185beb40d429707f74164bedc466f
Nicholas Ratti <nickratti@gmail.com>
2017-01-26 02:49:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    erb :team
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sg]
+    @pf = params[:pf]
+    @c = params[:c]
+  end
 
 end
