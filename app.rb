5bd1490d9ad224ab582d13a63dba8ef7b91c9728
HANA LEANDER <hana0070@gmail.com>
2017-02-05 18:08:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    @team_members = %w(name coach pg sg sf pf c) #%w = It's a notation to create an array of strings of the grouping
+    erb :newteam
+  end
 
+  post '/team' do
+    original_string = params['string']
+    @reversed_string = original_string.reverse
+    erb :newteam
+  end
 end
