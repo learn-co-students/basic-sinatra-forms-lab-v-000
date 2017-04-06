afecd93facb26451d8f75ba1750ecfaf979bc51a
mxdavis <md01@me.com>
2017-04-07 02:11:49 +0300

    Done.

--- app.rb
+++ app.rb
@@ -6,14 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/team' do
+  post '/newteam' do
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sg]
     @sf = params[:sf]
     @pf = params[:pf]
-    @center = params[:center]
+    @c = params[:c]
     erb :team
   end
 
