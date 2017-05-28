0aac8ffc5b52fc9ad09d5fd6ddcbd6bbeae41c11
Menachem Hornbacher <me@mendel.tech>
2017-05-28 23:09:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  POSITIONS = {
+    :name => "Team Name",
+    :coach => "Coach",
+    :pg => "Point Guard",
+    :sg => "Shooting Guard",
+    :sf => "Small Forward",
+    :pf => "Power Forward",
+    :c => "Center"
+  }
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    erb :team
+  end
 end
