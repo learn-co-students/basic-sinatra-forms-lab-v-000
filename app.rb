4146df95403510a56eaf280ae5a3ffeec2e1807c
blinded93 <blinded93@gmail.com>
2017-05-30 12:28:51 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+    # binding.pry
+  end
 
-
+  post '/newteam' do
+    @params = Struct.new(*params.keys).new(*params.values)
+    erb :team
+  end
 end
