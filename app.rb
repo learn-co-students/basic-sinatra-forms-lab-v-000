7cfaf8bcc3a90238049f5f58e1f92ae17b79cd1b
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-06-22 18:16:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
-
+require "pry"
 class App < Sinatra::Base
 
+  get "/newteam" do
+    erb :newteam
+  end
 
+  post "/team" do
+    @team = params
+    erb :team
+  end
 end
