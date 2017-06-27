3f83a43aa2c76f7c3b9128c976075b0faaabc3e0
Aimee Carney <acarney29@gmail.com>
2017-06-27 00:30:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/newteam" do
+    erb :newteam
+  end
+
+  post "/team" do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :team
+  end
 
 end
