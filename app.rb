2b62aa4774ebc561b30ac108f74378997e4167dd
charlie lee <ctlee9096@gmail.com>
2017-07-09 21:48:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,11 +3,21 @@ require 'sinatra/base'
 require 'pry'
 
 class App < Sinatra::Base
-
-  post '/newteam' do
-    binding.pry
-    @list= [params[:name], params[:coach], params[:pg], params[:sg], params[:sf], params[:pf], params[:c]]
-    binding.pry
+  get '/newteam' do
     erb :newteam
   end
+
+  post '/newteam' do
+    #binding.pry
+    @name = params[:name]
+    @coach= params[:coach]
+    @pg= params[:pg]
+    @sg= params[:sg]
+    @sf= params[:sf]
+    @pf= params[:pf]
+    @c = params[:c]
+    #binding.pry
+    erb :team
+  end
+
 end
