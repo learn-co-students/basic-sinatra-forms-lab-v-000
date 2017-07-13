5b5c79ceae96cbf849f7a1304fcb6503979bffc7
Susanna Klosko <sdklos@gmail.com>
2017-07-13 21:42:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "Hello World"
+  end
+
+  get '/newteam' do
+
+    erb :newteam
+  end
+
+  post '/team' do
+
+    erb :team
+  end
 
 end
