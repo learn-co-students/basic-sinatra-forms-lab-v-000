ff5e824b69ec5f8bbe36a3b1e33cf43a1f829b1e
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-01 20:29:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,30 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    'This is the root-page'
+  end
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    erb :team
+  end
+
+  post '/team' do
+    erb :team
+  end
+
 
 end
