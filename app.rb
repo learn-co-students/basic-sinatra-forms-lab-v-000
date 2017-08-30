346d0a47722b888b349bb0725004334e148447e8
Tyler <tbuchhei@alumni.nd.edu>
2017-08-30 13:10:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    @fields = {
+      name: "Team Name",
+      coach: "Coach",
+      pg: "Point Guard",
+      sg: "Shooting Guard",
+      sf: "Small Forward",
+      pf: "Power Forward",
+      c: "Center"
+    }
+    erb :newteam
+  end
+
+  post '/team' do
+    @info = params
+    erb :team
+  end
 
 end
