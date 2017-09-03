644369ecef2940b321ec2f20d47d8da3689b0f71
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-09-03 00:01:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @info = {
+      "Team Name" => params[:name],
+      "Coach" => params[:coach],
+      "Point Guard" => params[:pg],
+      "Shooting Guard"=> params[:sg],
+      "Power Forward" => params[:pf],
+      "Small Forward"=> params[:sf],
+      "Center"=> params[:c]
+    }
+    erb :team
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 ENV["SINATRA_ENV"] = "test"
 require_relative '../config/environment'
 require 'rack/test'
-require 'capybara/poltergeist'
+# require 'capybara/poltergeist'
+require 'capybara/rspec'
+require 'capybara/dsl'
 
-Capybara.javascript_driver = :poltergeist
+# Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
   config.include Capybara::DSL
