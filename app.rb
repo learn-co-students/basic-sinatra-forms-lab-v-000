d2054218a9938dcdf1ab88f91a807d849a9bc7bf
Johan Esteves <jsesteves91@gmail.com>
2017-10-08 20:25:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    params.each do |key, value|
+      instance_variable_set("@#{key}", value)
+    end
+    erb :team
+  end
 
 end
