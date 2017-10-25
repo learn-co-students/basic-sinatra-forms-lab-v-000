fde1379213838c53a2dc412a2981f675a1aa6c5e
lauraszyms <lauraszyms@gmail.com>
2017-10-24 17:26:20 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    params.to_s
+    erb :team
+  end
 
 
 end
