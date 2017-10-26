1522c7be1a3f2466c82bb7a85ec3d5770cc14fe9
Jacilyn Hayden <jacihayden@gmail.com>
2017-10-26 04:55:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
-require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/newteam' do
+    @teamname = params["name"]
+    @coach = params["coach"]
+    @pointguard = params["pg"]
+    @shootingguard = params["sg"]
+    @powerforward = params["pf"]
+    @smallforward = params["sf"]
+    @center = params["c"]
 
+    erb :team
+  end
+
+  post '/team' do
+
+    erb :team
+  end
 end
