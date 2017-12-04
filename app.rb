d8c8f9600a8e418ae0ced12d7d7831291a0bdcc3
Yujun Sung <yujunsung@gmail.com>
2017-12-04 03:27:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get "/newteam" do
+    erb :newteam
+  end
+  post "/team" do
+    @new_coach = params[:coach]
+    @new_sg = params[:sg]
+    @new_pg = params[:pg]
+    @new_sf = params[:sf]
+    @new_pf = params[:pf]
+    @new_name = params[:name]
+    @new_center = params[:c]
+    erb :team
+  end
+
 
 
 end
