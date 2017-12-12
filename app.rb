61c8dbc1ec7f8c274abde8901f06140945c10e23
Michael Evans <michael@michaelgregoryevans.com>
2017-12-12 21:34:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @power_forward = params[:pf]
+    @small_forward = params[:sf]
+    @center = params[:c]
+
+    erb :team
+  end
 
 end
