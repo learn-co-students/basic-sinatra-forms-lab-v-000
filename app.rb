8bf4d34e1666f946a182d43ddb9ca2d41106d41f
Cristina Collazos <clcgalex@gmail.com>
2018-01-04 19:48:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/" do
+    "Basic Sinatra Forms"
+  end
 
+  get "/newteam" do
+    erb :newteam
+  end
+
+  post "/team" do
+     @name = params[:name]
+     @coach = params[:coach]
+     @pg = params[:pg]
+     @sg = params[:sg]
+     @sf = params[:sf]
+     @pf = params[:pf]
+     @c = params[:c]
+    erb :team
+  end
 end
