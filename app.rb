b85547e647f2570c856441475b231b7fadabfb95
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 17:24:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,8 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    #So I could have simply referred to each of these as params[:name] etc. instead of making them variables and then inserting them.
+    #See line 9 of team.erb(it also works).
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
