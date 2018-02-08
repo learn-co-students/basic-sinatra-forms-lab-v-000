6da57d952ae602f7896670b231e47d30abdbdf02
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-08 16:29:35 -0500

    Rspeclemented post '/team'. ALL TESTS PASSING

--- app.rb
+++ app.rb
@@ -5,4 +5,15 @@ class App < Sinatra::Base
         erb :newteam
     end
 
+    post '/team' do
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @center = params[:c]
+
+        erb :team
+    end
 end
