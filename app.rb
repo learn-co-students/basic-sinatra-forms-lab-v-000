a3f1739623bbc30d27a9e9648f2868fd4dd0e7c6
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-13 23:17:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    erb :team
-  end
+   @name = params[:name]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg]
+   @pf = params[:pf]
+   @sf = params[:sf]
+   @c = params[:c]
+   erb :team
+ end
 
 
 end
