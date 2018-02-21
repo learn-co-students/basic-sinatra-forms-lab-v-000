4838021a4d92bf946d40cefa9a908c9109337ad8
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-02-21 03:50:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'sinatra/base'
-
+#========================================== 
 class App < Sinatra::Base
-
-
+#==================routes================== 
+  # get
+#------------------------------------------ 
+  get "/newteam" do
+  	erb :newteam
+  end
+#------------------------------------------ 
+  # post
+#------------------------------------------  
+  post '/team' do
+    puts params
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
+#========================================== 
 end
