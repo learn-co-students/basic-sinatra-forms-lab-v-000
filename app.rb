bd7f9558c4627a4591f77f723e65d337758aee76
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 20:30:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @full = {
+      name: 'Team Name',
+      coach: 'Coach',
+      pg: 'Point Guard',
+      sg: 'Shooting Guard',
+      sf: 'Small Forward',
+      pf: 'Power Forward',
+      c: 'Center'
+    }
+    erb :team
+  end
 
 end
