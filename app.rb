30bcb42f16bde546f12c84523424b280017c0e4e
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-19 17:10:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,13 +6,16 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/newteam' do
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]
+
+  post '/team' do
+
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
