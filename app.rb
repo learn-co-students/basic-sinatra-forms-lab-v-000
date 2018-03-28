04ff0efbf861650fbbab86db013d5b16202abbfd
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-28 20:36:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+  get '/newteam' do
+    erb :newteam
+  end
+  post '/team' do
+    @teams = [:newteam] #in the example from the previous code along, they set an array to an instance variable and then used that in the erb; unsure if that will work here.
+    erb :team
+  end
 end
