5079efdcdedea1f1b95a07f34cb2bbed1ffcf5b5
Sam. O <samrey2018@gmail.com>
2018-04-09 17:39:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
-       get'/' do
-         erb :team
+#get infor the server.=> to Browser.. (send to view)
+       get'/newteam' do
+         erb :newteam
        end
 
-
-       get'/newteam' do
-          erb :newteam
+# post infor Browser => server.
+       post'/team' do
+         @name = params["name"]
+         @coach = params["coach"]
+         @pg = params["pg"]
+         @sg = params["sg"]
+         @sf = params["sf"]
+         @pf = params["pf"]
+         @c = params["c"]
+          erb :team
        end
 
 end
+
+# Notes   Create forms in newteam.erb
+  # Create route in apps the responds to /team in newteam.erb
+  # use those created documents in  team.erb
