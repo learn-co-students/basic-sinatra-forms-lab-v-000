ccf7861d1f20bff342cc2394e1c5c0c83e82c600
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-10 18:14:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,4 +5,15 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
+
 end
