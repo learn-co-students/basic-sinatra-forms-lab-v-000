437854978d83e3cf08aa7458e8a15d40174eb9ad
Zac Willmington <zac.willmington@gmail.com>
2018-05-10 00:55:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get "/newteam" do
+        erb :newteam
+    end
 
+    post "/team" do
+         @name = params[:name]
+         @coach = params[:coach]
+         @pointGuard = params[:pg]
+         @shootingGuard = params[:sg]
+         @powerForward = params[:pf]
+         @smallForward = params[:sf]
+         @center = params[:c]
+        erb :team
+    end
 end
