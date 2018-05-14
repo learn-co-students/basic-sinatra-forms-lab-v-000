e09353487f32a24b26387b8672e7bdf8e0560664
David Ritchey <ritchey0713@gmail.com>
2018-05-14 22:01:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+  
+  get '/newteam' do 
+    
+    erb :newteam 
+  end 
+  
+   post '/team' do 
+      @coach = params[:coach]
+      @name = params[:name]
+      @pg = params[:pg]
+      @sg = params[:sg]
+      @pf = params[:pf]
+      @sf = params[:sf]
+      @c = params[:c]
+      
+      erb :team
+  end 
+  
+  
 
 
 end
