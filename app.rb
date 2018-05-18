310346268dda648f42a5211125aceb0e79de56ce
Kriti Rai <kriti.rai@gmail.com>
2018-05-18 17:48:47 +0000

    Updated team.erb and app.rb

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @input = params
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+
     erb :team
   end
 end
