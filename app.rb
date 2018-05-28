393a264611dbf8d1ad0e2228227055bc80ae987f
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-28 13:50:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    @@team = params
+    self
+    # binding.pry
+    erb :team
+  end
+
+  get '/team' do
+    erb :team
+  end
 
 end
