1c38f1e1332e935dde0bb68c118a83f296a2d0e0
Brett Weisberg <weisberg.brett@gmail.com>
2018-06-20 02:07:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,17 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/newteam' do
+    @team_name = params["team_name"]
+    @coach = params["coach"]
+    @pg = params["point_guard"]
+    @sg = params["shooting_guard"]
+    @sf = params["small_forward"]
+    @pf = params["power_forward"]
+    @center = params["center"]
+
+    erb :team
+  end
+
+
 end
