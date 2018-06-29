949d5ff4e19d17bb3326ef906607f74b1b2d5320
Ron Sala <ron.sala@gmail.com>
2018-06-29 13:29:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    binding.pry
+    @name = params[:name]
+    erb :team
+  end
 
 end
