9473131799a92cb306f875b630ec9503720f8c8e
Ron Sala <ron.sala@gmail.com>
2018-06-29 14:01:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    binding.pry
     @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
   end
 
