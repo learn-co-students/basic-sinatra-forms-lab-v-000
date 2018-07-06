7b7f48b6f74aa40bac8712a5bc44c5b5013cf7eb
Rebecca Tong <rtong@buffalo.edu>
2018-07-06 03:40:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,8 +3,17 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-
+    erb :team
   end
 
+  post '/team' do
+    "Team Name: #{params[:name]} /n
+    Coach: #{params[:coach]} /n
+    Point Guard: #{params[:pg]} /n
+    Shooting Guard: #{params[:sg]} /n
+    Power Forward: #{params[:pf]} /n
+    Small Forward: #{params[:sf]} /n
+    Center: #{params[:c]}"
+  end
 
 end
