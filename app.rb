e5c160cac8c192c4522418d45510c715ace325ae
Elaine Anderson <eparie@bu.edu>
2018-07-15 23:19:47 +0000

    team form

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
+
+  get '/newteam' do
+  erb :newteam
+end
+
+# Add your post route and action below
+
+post '/newteam' do
+ @new_team = params.values
+
+  erb :team
+end
+
 
 
 end
