ca9a42a2c6ef0a94b53aeb98c14b5afb9cf2bebf
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-01 21:12:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @params = params
+    # binding.pry
+    erb :team
+  end
 end
