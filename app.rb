55ae803d183573c73d5a01b4951540b3dea9b1d0
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-18 03:07:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @teamname = params[:name]
+    @coach = params[:coach]
+    @point = params[:pg]
+    @shooting = params[:sg]
+    @power = params[:pf]
+    @small = params[:sf]
+    @center = params[:c]
+    erb :team
+  end
+
 
 end
