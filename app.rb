dc581eb7750f0dee60d8b618ce8a89cad6d210af
Wasif Khan <waasif.khaan@gmail.com>
2018-08-31 21:59:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-
-end
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/team' do
+    @team_name = params[:Team_name]
+    @coach = params[:coach]
+    @point_guard=params[:Point_guard]
+    @shooting_guard=params[:Shooting_guard]
+    @small_forward=params[:Small_forward]
+    @power_forward=params[:Power_forward]
+    @center=params[:Center]
+    erb :team
+  end
+end 
