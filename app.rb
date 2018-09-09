c911ef5ec7050b98a326463cb0b3708ff2ee96f2
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-09 06:32:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = "Team Name: #{params[:name]}"
+    @coach = "Coach: #{params[:coach]}"
+    @pg = "Point Guard: #{params[:pg]}"
+    @sg = "Shooting Guard: #{params[:sg]}"
+    @sf = "Small Forward: #{params[:sf]}"
+    @pf = "Power Forward: #{params[:pf]}"
+    @c = "Center: #{params[:c]}"
+
+    erb :team
+  end
 
 
 end
