c0b2273e179dcc94e00999cac17f953a51bcb1f4
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-01 19:45:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,5 +3,20 @@ require 'pry'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    "Team Name: #{params[team_name]}"
+    "Coach: #{params[coach]}"
+    "Team Name: #{params[team_name]}"
+    "Team Name: #{params[team_name]}"
+    "Team Name: #{params[team_name]}"
+    "Team Name: #{params[team_name]}"
+    "Team Name: #{params[team_name]}"
+
+  end
+
 
 end
