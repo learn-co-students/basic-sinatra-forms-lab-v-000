fa20d486d74052afde971bd65baa3a15ee8c60a5
Barbara Choy <barbchoy@gmail.com>
2018-10-29 23:52:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+  end
+
 end
