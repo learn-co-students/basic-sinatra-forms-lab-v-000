aaa5130fb2666a13edeb4f92a8ab6804a3bc4618
Jeremy <jeremy.emata@gmail.com>
2018-11-14 18:40:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
+  post '/team' do
+      @name = params[:name]
+      @coach = params[:coach]
+      @pg = params[:pg]
+      @sg = params[:sg]
+      @pf = params[:pf]
+      @sf = params[:sf]
+      @c = params[:c]
+      erb :team
+    end
 
-
+=begin
+removed as test require each item to be individually named
+  post '/team' do
+   @parameters = params
+    erb :team
+  end
+=end
 end
