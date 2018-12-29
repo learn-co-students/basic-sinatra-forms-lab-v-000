c2cec9dcbcd85980d9c72f381bd47dd2a7de3937
Rafael Lopez <lpz.raf@gmail.com>
2018-12-29 03:50:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    params["string"]
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
     erb :team
   end
 
