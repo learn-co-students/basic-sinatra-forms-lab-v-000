f97196e4de659dd84eb5a6be9d89e36d777055a2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-04 22:01:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    @roster = [params["name"], params["coach"], params["pg"], params["sg"], params["sf"], params["pf"], params["c"]]
+
+    erb :team
+  end
 
 end
