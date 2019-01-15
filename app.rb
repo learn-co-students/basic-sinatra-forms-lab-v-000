07a958c5f5953c5a7906ee818d307636368c7939
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-15 01:37:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
+# Note to Indy: I think this lab is done? But honestly, no idea without shotgun. Take a look later#
+
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/new_team' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    
+    erb :team
+  end
 
 end
