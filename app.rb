8388e5c79a26b1278520a66eb6b8295257358c44
yuanxizhang <yuanxi.zhang@gmail.com>
2019-01-25 12:59:32 -0800

    Add GET and POST routes

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+	get '/newteam' do 
+		erb :newteam.erb
+	end 
 
+	post '/team' do 
+		@teamname = params[:name]
+		@coach = params[:coach]
+		@pointguard = params[:pg]
+		@shootingguard = params[:sg]
+		@powerforward = params[:pf]
+		@smallforward = params[:sf]
+		@center = params[:c]
 
+		erb :team.erb
+	end
 end
