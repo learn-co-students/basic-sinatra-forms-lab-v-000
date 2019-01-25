01c1c878853d1ac9146c37121df7901f72aa0205
Heidi Hlavinka <hchlav@gmail.com>
2019-01-25 01:20:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ class App < Sinatra::Base
   end
 
   post "/team" do
+    @name = params[:name]
     @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+# binding.pry
     erb :team
   end
 
