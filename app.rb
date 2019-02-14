bc4e9740500518f96dc4b5b2cccaa761402dfe47
nanton83 <nicholas.romans@gmail.com>
2019-02-14 14:28:32 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,14 +7,14 @@ class App < Sinatra::Base
     end
 
     post '/newteam' do
-        "Team Name: #{params[:name]}"
-        "Coach: #{params[:coach]}"
-        "Point Guard: #{params[:pg]}"
-        "Shooting Guard: #{params[:sg]}"
-        "Small Forward: #{params[:sf]}"
-        "Power Forward: #{params[:pf]}"
-        "Center: #{params[:c]}"
-      end
+        "Team Name: #{params[:name]}
+        Coach: #{params[:coach]}
+        Point Guard: #{params[:pg]}
+        Shooting Guard: #{params[:sg]}
+        Small Forward: #{params[:sf]}
+        Power Forward: #{params[:pf]}
+        Center: #{params[:c]}"
+    end
 
 
 end
