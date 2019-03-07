d779af59275b6a5819cc1af91232d5f2551c65a5
Cheryl K <cherylkelson@icloud.com>
2019-03-07 20:33:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post 'team' do
+  post '/team' do
     @name = params[ :name]
     @coach = params[ :coach]
     @pg = params[ :pg]
@@ -13,14 +13,7 @@ class App < Sinatra::Base
     @pf = params[ :pf]
     @sf = params[ :sf]
     @c = [ :c]
-    
+      erb :team 
   end
-
-
-
-
-
-
-
 
 end
