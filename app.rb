3e4640540f6d6d8ecbef4c0df9a52aee79c7da92
Sara Walker <sarawall1187@gmail.com>
2019-03-09 22:42:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 end
