c94d61cbddffe0ee919c10b0fe0a518e385d146e
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-03-13 06:33:20 +0000

    saving tests passed so far

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    # params[:name]
+    # @name
+    erb :team
+  end
 
 end
