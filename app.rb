56a9b1283d3d949cc558265fcbb938dbd256fcd5
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-03-14 03:37:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,22 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  # get '/team' do
+  #   # TEST
+  #   # erb:team
+  # end
+
   post '/team' do
+    # binding.pry
     # params[:name]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    # @name
     # @name
     erb :team
   end
