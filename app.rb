f7b10271673096064c87b4c9d0742556bd750b5a
Laura Messa Travin <laura.travin@gmail.com>
2019-03-22 20:15:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+      erb :newteam
+    end
+
+    post '/newteam' do
+      binding.pry
+      @team = params
+      erb :team
+    end
+
 
 end
