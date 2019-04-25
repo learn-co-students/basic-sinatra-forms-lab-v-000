83f66d49def5dcbd9142c06a61cb6b775b88da65
Kat Tochowicz <ktochow1@binghamton.edu>
2019-04-25 18:07:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,32 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
 get '/newteam' do
   erb :newteam
 end
 
 post 'team' do
   @name = params[:name]
+=======
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/team' do
+    @name = params[:name]
+>>>>>>> ba799a017f54c852b413a1d8ce6d10a1f63d2f1b
     @coach = params[:coach]
     @pg = params[:pg]
     @pf = params[:pf]
     @sg = params[:sg]
     @c = params[:c]
+<<<<<<< HEAD
     erb :team
 end
 
+=======
+    erb :team 
+  end
+>>>>>>> ba799a017f54c852b413a1d8ce6d10a1f63d2f1b
 end
