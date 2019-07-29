da5ee44649b80c53194876ab9f9db29828c121eb
Marcus Edwards 111 <Soulo@Marcuss-MacBook-Pro.local>
2019-07-28 19:09:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,5 +6,19 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/team' do
+    @array = [@team = params[:name], @coach = params[:name], @point = params[:pg], @shooting = params[:sg], @power = params[:pf], @small = params[:sf], @center = params[:c]]
+    @element1 = @array[0]
+    @arr2 = @array
+
+    erb :team
+  end
+
+  get '/team' do
+    erb :team
+  end
+
+
+
 
 end
