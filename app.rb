c6e8d9b2756390d2985bcb9c557193b37a89fafb
Karen Flores <karen.ag.flores@gmail.com>
2019-10-09 22:06:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/newteam' do
+    @teamname = params["name"]
+    @coach = params["coach"]
+    @point = params["pg"]
+    @shootg = params["sg"]
+    @powerf = params["pf"]
+    @smallf = params["sf"]
+    @center = params["c"]
+    erb :team
+  end
+
 
 end
