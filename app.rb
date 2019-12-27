f9bb7fe784ed306e9a7480c4be9f358978f5eb0d
Olivia Kesler <oakesler@protonmail.ch>
2019-12-27 06:09:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-   get '/newteam' do
+  
+  get '/newteam' do
     erb :newteam
+  end
+  
+  post '/team' do
+    @coach = params["Coach"]
+    @point_guard = params["Point Guard"]
+    @shooting_guard = params["Shooting Guard"]
+    @small_forward = params["Small Forward"]
+    @power_forward = params["Power Forward"]
+    @center = params["Center"]
+    
+    erb :team
+  end
+    
+    
+    
+    
+    
+    
   end
 
 
