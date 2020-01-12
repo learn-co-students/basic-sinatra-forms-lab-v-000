407d2bac7a9678386dca3e08c6587ffea321b73a
Jonathan Fernandez <jfern20@gmail.com>
2020-01-12 01:33:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,21 +2,36 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
  
- get "/newteam" do 
-  erb :newteam
-end 
+ #get "/newteam" do 
+  #erb :newteam
+#end 
 
-  post "/team" do 
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]  #center #all params as strings!!!
+  #post "/team" do 
+    #@name = params["name"]
+    #@coach = params["coach"]
+    #@pg = params["pg"]
+    #@sg = params["sg"]
+    #@sf = params["sf"]
+    #@pf = params["pf"]
+    #@c = params["c"]  #center #all params as strings!!!
     
-   erb :team 
+   #erb :team 
+ #end 
+ 
+ 
+ get '/newteam' do 
+   erb :newteam 
  end 
  
-  
+ 
+ post '/team' do
+   @first = params["first"]
+   @second = params["second"]
+   @third = params["third"]
+   @forth = params["forth"]
+   @fifth = params["fifth"]
+   
+   erb :team
+ end 
+ 
 end
