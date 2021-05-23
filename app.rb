c3a2324fa6d17d35d96203c1b8b2663c9bf2a263
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-23 18:56:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do 
 
+    erb :newteam
+  end
 
+#   post '/team' do
+#    @name = params[:name]
+#     # "#{params[:coach]}"
+#     # "#{params[:pg]}"
+#     # "#{params[:sg]}"
+#     # "#{params[:sf]}"
+#     # "#{params[:pf]}"
+
+#     erb :team
+#   end
 end
+
+
