a49fabed1ec1d303e63101d50339c3e00ac2db61
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-23 23:07:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/newteam' do 
+    
     erb :newteam
   end
 
   post '/team' do
-#    @name = params[:name]
+    # @name = params[:name]
+#    "Team Name: #{params[:name]}"
     # "#{params[:coach]}"
     # "#{params[:pg]}"
     # "#{params[:sg]}"
